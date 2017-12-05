20d3ec1417f5830fab4bf550854cd0d5f247e50c
barotfi <barotfi@yahoo.com>
2017-12-05 17:23:05 +0100

    javitas5

--- mymall/settings.py
+++ mymall/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = '&e@1tuh%*qy$k!3_=t)rlblpb1i3^2g0=!gb=pzr_*wq65+9q+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-'barotfi.pythoneverywhere.com'
-]
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
